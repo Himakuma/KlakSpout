7eb6f07f98cef517246ced1ce1a06e43667e36aa
kumatta <ssapp.devuser@gmail,com>
2025-02-15 17:12:28 +0900

    すでにフォーマットがあったので、修正

--- Packages/jp.keijiro.klak.spout/Runtime/Internal/Receiver.cs
+++ Packages/jp.keijiro.klak.spout/Runtime/Internal/Receiver.cs
@@ -1,99 +1,108 @@
+using System;
 using UnityEngine;
-using System.Runtime.InteropServices;
 using IntPtr = System.IntPtr;
 
-namespace Klak.Spout {
-
+namespace Klak.Spout
+{
 //
 // Wrapper class for receiver instances on the native plugin side
 //
-sealed class Receiver : System.IDisposable
-{
-    #region Public property
-
-    public Texture2D Texture => _texture;
-
-    #endregion
-
-    #region Private objects
-
-    IntPtr _plugin;
-    EventKicker _event;
-    Texture2D _texture;
-
-    #endregion
-
-    #region Object lifecycle
-
-    public Receiver(string sourceName)
+    internal sealed class Receiver : IDisposable
     {
-        if (string.IsNullOrEmpty(sourceName)) return;
+        #region Public property
 
-        // Plugin object allocation
-        _plugin = Plugin.CreateReceiver(sourceName);
-        if (_plugin == IntPtr.Zero) return;
+        public Texture2D Texture { get; private set; }
 
-        // Event kicker (heap block for interop communication)
-        _event = new EventKicker(new EventData(_plugin));
+        #endregion
 
-        // Initial update event
-        _event.IssuePluginEvent(EventID.UpdateReceiver);
-    }
+        #region Frame update method
 
-    public void Dispose()
-    {
-        if (_plugin != IntPtr.Zero)
+        public void Update()
         {
-            // Isssue the closer event to destroy the plugin object from the
-            // render thread.
-            _event.IssuePluginEvent(EventID.CloseReceiver);
+            if (_plugin == IntPtr.Zero)
+            {
+                return;
+            }
 
-            // Event kicker (interop memory) deallocation:
-            // The close event above will refer to the block from the render
-            // thread, so we actually can't free the memory here. To avoid this
-            // problem, EventKicker uses MemoryPool to delay the memory
-            // deallocation by the end of the frame.
-            _event.Dispose();
+            var data = Plugin.GetReceiverData(_plugin);
 
-            _plugin = IntPtr.Zero;
+            // Texture refresh:
+            // If we are referring to an old texture pointer, destroy it first.
+            if (Texture != null &&
+                Texture.GetNativeTexturePtr() != data.texturePointer)
+            {
+                Utility.Destroy(Texture);
+                Texture = null;
+            }
+
+            // Lazy initialization:
+            // We try creating a receiver texture every frame until getting a
+            // correct one.
+            if (Texture == null && data.texturePointer != IntPtr.Zero)
+            {
+                Texture = Texture2D.CreateExternalTexture
+                ((int)data.width, (int)data.height, TextureFormat.RGBA32,
+                    false, false, data.texturePointer);
+            }
+
+            // Update event for the render thread
+            _event.IssuePluginEvent(EventID.UpdateReceiver);
         }
 
-        Utility.Destroy(_texture);
-        _texture = null;
-    }
+        #endregion
 
-    #endregion
+        #region Private objects
 
-    #region Frame update method
+        private IntPtr _plugin;
+        private readonly EventKicker _event;
 
-    public void Update()
-    {
-        if (_plugin == IntPtr.Zero) return;
+        #endregion
 
-        var data = Plugin.GetReceiverData(_plugin);
+        #region Object lifecycle
 
-        // Texture refresh:
-        // If we are referring to an old texture pointer, destroy it first.
-        if (_texture != null &&
-            _texture.GetNativeTexturePtr() != data.texturePointer)
+        public Receiver(string sourceName)
         {
-            Utility.Destroy(_texture);
-            _texture = null;
+            if (string.IsNullOrEmpty(sourceName))
+            {
+                return;
+            }
+
+            // Plugin object allocation
+            _plugin = Plugin.CreateReceiver(sourceName);
+            if (_plugin == IntPtr.Zero)
+            {
+                return;
+            }
+
+            // Event kicker (heap block for interop communication)
+            _event = new EventKicker(new EventData(_plugin));
+
+            // Initial update event
+            _event.IssuePluginEvent(EventID.UpdateReceiver);
         }
 
-        // Lazy initialization:
-        // We try creating a receiver texture every frame until getting a
-        // correct one.
-        if (_texture == null && data.texturePointer != IntPtr.Zero)
-            _texture = Texture2D.CreateExternalTexture
-              ((int)data.width, (int)data.height, TextureFormat.RGBA32,
-               false, false, data.texturePointer);
+        public void Dispose()
+        {
+            if (_plugin != IntPtr.Zero)
+            {
+                // Isssue the closer event to destroy the plugin object from the
+                // render thread.
+                _event.IssuePluginEvent(EventID.CloseReceiver);
 
-        // Update event for the render thread
-        _event.IssuePluginEvent(EventID.UpdateReceiver);
+                // Event kicker (interop memory) deallocation:
+                // The close event above will refer to the block from the render
+                // thread, so we actually can't free the memory here. To avoid this
+                // problem, EventKicker uses MemoryPool to delay the memory
+                // deallocation by the end of the frame.
+                _event.Dispose();
+
+                _plugin = IntPtr.Zero;
+            }
+
+            Utility.Destroy(Texture);
+            Texture = null;
+        }
+
+        #endregion
     }
-
-    #endregion
-}
-
 } // namespace Klak.Spout
--- Plugin/Receiver.h
+++ Plugin/Receiver.h
@@ -20,6 +20,8 @@ namespace KlakSpout
 
     void update()
     {
+      std::printf("\nDD:update\n");
+
       // Search the Spout name list.
       unsigned int width, height;
       HANDLE handle;
@@ -28,44 +30,34 @@ namespace KlakSpout
                      .CheckSender(_name.c_str(), width, height, handle, format);
 
       // Do nothing further if the current texture is valid.
-      if (res && _texture && _width == width && _height == height)
+      if (res && _texture && _width == width && _height == height && _dxigformat == format)
         return;
 
       HRESULT hres;
 
       if (_system->isD3D12)
       {
+        std::printf("\nKlakSpout: DX12\n");
+
         // Handle -> D3D12Resource
         WRL::ComPtr<ID3D12Resource> resource;
         hres = _system->getD3D12Device()
                    ->OpenSharedHandle(handle, IID_PPV_ARGS(&resource));
         _texture = resource;
-        if (resource)
-        {
-          D3D12_RESOURCE_DESC desc = resource->GetDesc();
-          _dxgiFormat = static_cast<unsigned int>(desc.Format);
-        }
       }
       else
       {
+        std::printf("\nKlakSpout: DX11\n");
         // Handle -> D3D11Resource
         WRL::ComPtr<ID3D11Resource> resource;
         hres = _system->getD3D11Device()
                    ->OpenSharedResource(handle, IID_PPV_ARGS(&resource));
         _texture = resource;
-
-        WRL::ComPtr<ID3D11Texture2D> texture2D;
-        hres = resource.As(&texture2D);
-        if (SUCCEEDED(hres))
-        {
-          D3D11_TEXTURE2D_DESC desc;
-          texture2D->GetDesc(&desc);
-          _dxgiFormat = static_cast<unsigned int>(desc.Format);
-        }
       }
 
       _width = width;
       _height = height;
+      _dxigformat = format;
 
       if (FAILED(hres))
         LogError("OpenSharedResource", _name, hres);
@@ -76,19 +68,23 @@ namespace KlakSpout
     struct InteropData
     {
       unsigned int width, height;
-      unsigned int dxgiFormat;
+      unsigned int dxigformat;
       void *texture_pointer;
     };
 
     InteropData getInteropData() const
     {
-      return InteropData{.width = _width, .height = _height, dxgiFormat = _dxgiFormat, .texture_pointer = _texture.Get()};
+      return InteropData{
+          .width = _width,
+          .height = _height,
+          .dxigformat = _dxigformat,
+          .texture_pointer = _texture.Get()};
     }
 
   private:
     std::string _name;
     unsigned int _width, _height;
-    unsigned int _dxgiFormat;
+    DWORD _dxigformat;
     WRL::ComPtr<IUnknown> _texture;
   };
 
